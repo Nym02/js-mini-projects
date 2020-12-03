53f4fe00656253ec324bc75f50d0fc92c0c49b7a
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-12-03 16:31:57 +0600

    random hex color generator

--- randomHexColor/app.js
+++ randomHexColor/app.js
@@ -2,6 +2,7 @@ let hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
 let color = document.querySelector('.color');
 let btn = document.getElementById('btn');
+let bg = document.getElementById('bg');
 
 
 
@@ -12,6 +13,7 @@ btn.addEventListener('click', ()=>{
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
+    bg.style.backgroundColor = hexColor;
 })
 
 
